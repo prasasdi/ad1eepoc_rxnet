0fd28d6ccaf93cab79309e3f14c2daecb3d2cdcc
prasasdi <rizky.adie7@gmail.com>
2025-01-14 20:31:48 +0700

    Disposable scheduler

---
+++ MainAplikasi/DisposableScheduler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAplikasi
+{
+    public class DisposableScheduler
+    {
+        public IObservable<int> CreateObservable()
+        {
+            IObservable<int> ob = Observable.Create<int>(o =>
+            {
+                var cancel = new CancellationDisposable(); // internally creates a new CancellationTokenSource
+                NewThreadScheduler.Default.Schedule(() =>
+                {
+                    int i = 0;
+                    for (; ; )
+                    {
+                        Thread.Sleep(200);  // here we do the long lasting background operation
+                        if (!cancel.Token.IsCancellationRequested)    // check cancel token periodically
+                            o.OnNext(i++);
+                        else
+                        {
+                            Console.WriteLine("Aborting because cancel event was signaled!");
+                            o.OnCompleted();
+                            return;
+                        }
+                    }
+                }
+                );
+
+                return cancel;
+            });
+
+            return ob;
+        }
+    }
+}
--- MainAplikasi/Program.cs
+++ MainAplikasi/Program.cs
@@ -10,3 +10,11 @@ observerOnDemand.DoLongRunningOperation("Iki loh mulai");
 
 CombineLatest combineLatest = new CombineLatest();
 combineLatest.ParallelExecutionTest();
+
+DisposableScheduler scheduler = new DisposableScheduler();
+IDisposable subscription = scheduler.CreateObservable().Subscribe(i => Console.WriteLine(i));
+Console.WriteLine("Press any key to cancel");
+Console.ReadKey();
+subscription.Dispose();
+Console.WriteLine("Press any key to quit");
+Console.ReadKey();  // give background thread chance to write the cancel acknowledge message
