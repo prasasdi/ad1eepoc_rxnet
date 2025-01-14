5f2067a2298836e99d9fb4f5f31a3a6416c4a231
prasasdi <rizky.adie7@gmail.com>
2025-01-14 20:04:14 +0700

    Add project files.

---
+++ MainAplikasi/BackgroundWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAplikasi
+{
+    public static class BackgroundWork
+    {
+        public static void StartBackgroundWork()
+        {
+            Console.WriteLine("Shows use of Start to start on a background thread:");
+            var o = Observable.Start(() =>
+            {
+                //This starts on a background thread.
+                Console.WriteLine("From background thread. Does not block main thread.");
+                Console.WriteLine("Calculating...");
+                Thread.Sleep(3000);
+                Console.WriteLine("Background work completed.");
+            }).Finally(() => Console.WriteLine("Main thread completed."));
+            Console.WriteLine("\r\n\t In Main Thread...\r\n");
+            o.Wait();   // Wait for completion of background operation.
+        }
+    }
+}
